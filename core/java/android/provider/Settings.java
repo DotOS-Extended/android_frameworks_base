dirtyredmi2/platform_frameworks_base

Disable dashboard suggestions [2/2]
Change-Id: I33879e9b8e1ef15898e781aad4766b755df36287

core/java/android/provider/Settings.java
@@ -3794,6 +3794,12 @@ public final class Settings {
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * Disable dashboard suggestions in settings
+         * @hide
+         */
+        public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
+
+        /**
          * Volume rocker wake
          * @hide
          */

DotOS-Extended/android_frameworks_base

core/java/android/provider/Settings.java
@@ -3738,6 +3738,12 @@ public final class Settings {
         };
 
         /**
+         * Disable dashboard suggestions in settings
+         * @hide
+         */
+        public static final String ENABLE_SUGGESTIONS = "enable_suggestions";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate